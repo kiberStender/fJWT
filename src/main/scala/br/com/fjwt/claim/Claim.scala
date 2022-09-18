5c8b4b057d83fc89837617bef7035892129283aa
Kleber Stender <kleber.stender@gmail.com>
2022-09-18 12:03:06 +0200

    Created Claim case class to control the specific parts of the code

---
+++ src/main/scala/br/com/fjwt/claim/Claim.scala
@@ -0,0 +1,44 @@
+package br.com.fjwt
+package claim
+
+import io.circe.*, io.circe.parser, io.circe.syntax.*
+import io.circe.Decoder.Result
+
+final case class Claim[P: Codec](
+    iss: Option[String],
+    sub: Option[String],
+    aud: Option[String],
+    exp: Option[Long],
+    nbf: Option[Long],
+    iat: Option[Long],
+    jti: Option[String],
+    payload: P
+)
+
+object Claim:
+  given encoder[P: Codec](using EP: Encoder[P]): Encoder[Claim[P]] =
+    new Encoder[Claim[P]]() {
+      def apply(cp: Claim[P]): Json = Json.obj(
+        "iss" -> cp.iss.asJson,
+        "sub" -> cp.sub.asJson,
+        "aud" -> cp.aud.asJson,
+        "exp" -> cp.exp.asJson,
+        "nbf" -> cp.nbf.asJson,
+        "iat" -> cp.iat.asJson,
+        "jti" -> cp.jti.asJson,
+        "payload" -> EP(cp.payload)
+      )
+    }
+  given decoder[P: Codec]: Decoder[Claim[P]] = new Decoder[Claim[P]]() {
+    def apply(c: HCursor): Result[Claim[P]] =
+      for
+        iss <- c.downField("iss").as[Option[String]]
+        sub <- c.downField("sub").as[Option[String]]
+        aud <- c.downField("aud").as[Option[String]]
+        exp <- c.downField("exp").as[Option[Long]]
+        nbf <- c.downField("nbf").as[Option[Long]]
+        iat <- c.downField("iat").as[Option[Long]]
+        jti <- c.downField("jti").as[Option[String]]
+        payload <- c.downField("payload").as[P]
+      yield Claim(iss, sub, aud, exp, nbf, iat, jti, payload)
+  }
