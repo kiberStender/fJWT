d582e1cb5d949ec9393d8a45c444600d331353ac
Kleber Stender <kleber.stender@gmail.com>
2022-09-25 10:51:27 +0200

    Changed alg value from JWTAlgorythm to HmacEncoderAlgorithms

--- src/main/scala/br/com/fjwt/JWTHeader.scala
+++ src/main/scala/br/com/fjwt/JWTHeader.scala
@@ -1,12 +1,15 @@
 package br.com.fjwt
 
 import io.circe.*, io.circe.generic.semiauto.*
+import br.com.fjwt.crypto.hs.HmacEncoderAlgorithms
 
 sealed trait JWTHeader
 case object EmptyHeader extends JWTHeader
-case class Alg(alg: JWTAlgorythm, typ: String) extends JWTHeader
+case class Alg(alg: HmacEncoderAlgorithms, typ: String) extends JWTHeader
 
 object JWTHeader {
+  given Codec[HmacEncoderAlgorithms] = io.circe.generic.semiauto.deriveCodec
+
   given encoder: Encoder[Alg] =
     Encoder.forProduct2("alg", "typ")(h => (h.alg.alg, h.typ))
   given decoder: Decoder[Alg] =
