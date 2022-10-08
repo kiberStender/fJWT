5cefdaa4cfc9730f01e8a3c5e38d4e9263df7e20
Kleber Stender <kleber.stender@gmail.com>
2022-10-08 17:01:09 +0200

    Created JWTError enum to classify all possible exceptions

---
+++ 01-domain/src/main/scala/br/com/fjwt/error/JWTError.scala
@@ -0,0 +1,12 @@
+package br.com.fjwt
+package error
+
+enum JWTError(val message: String):
+  case NotMappedError(errorMessage: String) extends JWTError(errorMessage)
+  case ExpiredToken extends JWTError("Token is expired")
+  case InvalidSignature extends JWTError("JWT Signature does not match")
+  case NullPrivateKey extends JWTError("Private key cannot null")
+  case EmptyPrivateKey extends JWTError("Private key cannot be empty")
+  case NullToken extends JWTError("Token cannot null")
+  case EmptyToken extends JWTError("Token cannot be empty")
+  case WrongTokenParts extends JWTError("Token has to has 3 parts: header.payload.signature")
