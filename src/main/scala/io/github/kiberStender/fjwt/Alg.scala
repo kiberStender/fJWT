69133286ba21f2649ca3ece00953f43b4676f6d5
Kleber Stender <kleber.stender@gmail.com>
2022-11-14 09:04:09 +0100

    Implemented a Decoder for HmacEncoderAlgorithms

--- src/main/scala/io/github/kiberStender/fjwt/Alg.scala
+++ src/main/scala/io/github/kiberStender/fjwt/Alg.scala
@@ -1,14 +1,12 @@
 package io.github.kiberStender
 package fjwt
 
-import io.circe.*
-import io.circe.generic.semiauto.*
+import io.circe.{Encoder, Decoder}
 import io.github.kiberStender.fjwt.crypto.hs.HmacEncoderAlgorithms
+import io.github.kiberStender.fjwt.crypto.hs.HmacEncoderAlgorithms.*
 private case class Alg(alg: HmacEncoderAlgorithms, typ: String)
 
 private object Alg:
-  given Codec[HmacEncoderAlgorithms] = io.circe.generic.semiauto.deriveCodec
-
   given encoder: Encoder[Alg] =
     Encoder.forProduct2("alg", "typ")(h => (h.alg.alg, h.typ))
   given decoder: Decoder[Alg] =
--- src/main/scala/io/github/kiberStender/fjwt/crypto/hs/HmacEncoderAlgorithms.scala
+++ src/main/scala/io/github/kiberStender/fjwt/crypto/hs/HmacEncoderAlgorithms.scala
@@ -3,10 +3,26 @@ package fjwt
 package crypto
 package hs
 
+import io.circe.{Decoder, DecodingFailure, HCursor}
+
 enum HmacEncoderAlgorithms(val alg: String):
-  case HS1 extends HmacEncoderAlgorithms("HmacSHA1")
-  case HS224 extends HmacEncoderAlgorithms("HmacSHA224")
-  case HS256 extends HmacEncoderAlgorithms("HmacSHA256")
-  case HS384 extends HmacEncoderAlgorithms("HmacSHA384")
-  case HS512 extends HmacEncoderAlgorithms("HmacSHA512")
-  case HMD5 extends HmacEncoderAlgorithms("HmacMD5")
+  case HmacSHA1 extends HmacEncoderAlgorithms("HmacSHA1")
+  case HmacSHA224 extends HmacEncoderAlgorithms("HmacSHA224")
+  case HmacSHA256 extends HmacEncoderAlgorithms("HmacSHA256")
+  case HmacSHA384 extends HmacEncoderAlgorithms("HmacSHA384")
+  case HmacSHA512 extends HmacEncoderAlgorithms("HmacSHA512")
+  case HmacMD5 extends HmacEncoderAlgorithms("HmacMD5")
+
+object HmacEncoderAlgorithms:
+  given Decoder[HmacEncoderAlgorithms] = (hCursor: HCursor) =>
+    for
+      value <- hCursor.as[String]
+      result <- value match
+        case "HmacSHA1"   => Right(HmacSHA1)
+        case "HmacSHA224" => Right(HmacSHA224)
+        case "HmacSHA256" => Right(HmacSHA256)
+        case "HmacSHA384" => Right(HmacSHA384)
+        case "HmacSHA512" => Right(HmacSHA512)
+        case "HmacMD5"    => Right(HmacMD5)
+        case s            => Left(DecodingFailure(s"Invalid algorithm $s", hCursor.history))
+    yield result
