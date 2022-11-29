9dfffad9b7963eaaa637e57894868ff09ae2e77c
Kleber Stender <kleber.stender@gmail.com>
2022-11-29 09:25:13 +0100

    Extracted key functions to create a new way of instantiating a JWTDecoder and created two distinct types of tests

--- src/main/scala/io/github/kiberStender/fjwt/JWTDecoder.scala
+++ src/main/scala/io/github/kiberStender/fjwt/JWTDecoder.scala
@@ -2,97 +2,58 @@ package io.github.kiberStender
 package fjwt
 
 import cats.MonadError
-import cats.syntax.all.{
-  catsSyntaxApplicativeErrorId,
-  catsSyntaxApplicativeId,
-  catsSyntaxEq,
-  toFlatMapOps,
-  toFunctorOps
+import cats.syntax.all.{toFlatMapOps, toFunctorOps}
+import io.circe.Codec
+import io.github.kiberStender.fjwt.utils.PayloadExtractor
+import io.github.kiberStender.fjwt.validation.StringValidation
+import io.github.kiberStender.fjwt.error.JWTError.{
+  NullPrivateKey,
+  EmptyPrivateKey,
+  NullToken,
+  EmptyToken
 }
 
-import io.circe.{Codec, parser}
+import java.time.ZoneId
 
-import io.github.kiberStender.fjwt.crypto.base64.Base64Decoder
-import io.github.kiberStender.fjwt.crypto.hs.{HmacEncoder, HmacEncoderAlgorithms}
-import io.github.kiberStender.fjwt.error.JWTError
-import io.github.kiberStender.fjwt.error.JWTError.*
-import io.github.kiberStender.fjwt.validation.StringValidation
-
-import java.time.{Instant, LocalDateTime, ZoneId}
-
-/**
- * A trait that describes the {@link JWTDecoder} typeclass
- * @tparam F A given container that wraps the return type
- */
+/** A trait that describes the {@link JWTDecoder} typeclass
+  *
+  * @tparam F
+  *   A given container that wraps the return type
+  */
 trait JWTDecoder[F[*]]:
-  /**
-   * The method to decode a given payload object described by the type P
-   * @param privateKey The private key previously used to encode the payload
-   * @param accessToken The JWT token that will be decoded
-   * @param ZoneId The {@link ZoneID} used in claim to make expired date more accurate
-   * @tparam P The type of the payload
-   * @return The payload object wrapped in F or an Error wrapped in F describing the problem
-   */
+  /** The method to decode a given payload object described by the type P
+    * @param privateKey
+    *   The private key previously used to encode the payload
+    * @param accessToken
+    *   The JWT token that will be decoded
+    * @param ZoneId
+    *   The {@link ZoneID} used in claim to make expired date more accurate
+    * @tparam P
+    *   The type of the payload
+    * @return
+    *   The payload object wrapped in F or an Error wrapped in F describing the problem
+    */
   def decode[P: Codec](privateKey: String)(accessToken: String)(using ZoneId): F[P]
 
-/**
- * Instance factory for {@link JWTDecoder}
- */
+/** Instance factory for {@link JWTDecoder}
+  */
 object JWTDecoder:
-  /**
-   * A method to instantiate an {@link JWTDecoder} that figures out the hmac algorithm by decoding the token header
-   * @param base64Decoder An instance of {@link Base64Decoder}
-   * @tparam F An instance of {@link MonadError[F, Throwable]}
-   * @return An instance of {@link JWTDecoder}
-   */
+  /** A method to instantiate a {@link JWTDecoder}
+    * @param payloadExtractor
+    *   An instance of {@link PayloadExtractor}
+    * @tparam F
+    *   An instance of {@link MonadError}
+    * @return
+    *   An instance of {@link JWTDecoder}
+    */
   def dsl[F[*]: [F[*]] =>> MonadError[F, Throwable]](
-      base64Decoder: Base64Decoder[F]
+      payloadExtractor: PayloadExtractor[F]
   ): JWTDecoder[F] = new JWTDecoder[F]:
     lazy val stringValidation: StringValidation[F] = StringValidation.dsl
-
-    private def isSignatureValid(encoded: String)(signature: String)(payload: String): F[String] =
-      if encoded === signature then base64Decoder decode payload
-      else InvalidSignature.raiseError[F, String]
-
-    private def has3Parts: Array[String] => F[(String, String, String)] =
-      case Array(header, payload, signature) => (header, payload, signature).pure[F]
-      case _ => WrongTokenParts.raiseError[F, (String, String, String)]
-
-    private def decodeHeader(header: String): F[HmacEncoder[F]] =
-      parser
-        .decode[Alg](header)
-        .map(_.alg)
-        .fold(_.raiseError[F, HmacEncoderAlgorithms], _.pure[F])
-        .map(HmacEncoder.hsEncoder)
-
-    private def extractPayload(privateKey: String)(parts: Array[String]): F[String] =
-      for
-        (header, payload, signature) <- has3Parts(parts)
-        decodedHeader <- base64Decoder.decode(header)
-        hmacDecoder <- decodeHeader(decodedHeader)
-        encoded <- hmacDecoder.encode(privateKey)(s"$header.$payload")
-        result <- isSignatureValid(encoded)(signature)(payload)
-      yield result
-
-    private def decodeClaim[P: Codec](payload: String): F[Claim[P]] =
-      parser
-        .decode[Claim[P]](payload)
-        .fold(throwable => NotMappedError(throwable.getMessage).raiseError[F, Claim[P]], _.pure[F])
-
-    private def isExpired[P: Codec](claim: Claim[P])(using zoneId: ZoneId): F[P] =
-      claim.exp
-        .map { exp =>
-          val localDateExp = Instant.ofEpochMilli(exp).atZone(zoneId).toLocalDateTime
-          if localDateExp isAfter LocalDateTime.now() then claim.payload.pure[F]
-          else ExpiredToken.raiseError[F, P]
-        }
-        .getOrElse(claim.payload.pure[F])
 
     def decode[P: Codec](privateKey: String)(accessToken: String)(using ZoneId): F[P] =
       for
         key <- stringValidation.validate(privateKey)(NullPrivateKey)(EmptyPrivateKey)
         token <- stringValidation.validate(accessToken)(NullToken)(EmptyToken)
-        payloadStr <- extractPayload(key)(token split "\\.")
-        decodedPayload <- decodeClaim(payloadStr)
-        payload <- isExpired(decodedPayload)
+        payload <- payloadExtractor.extract(key)(token)
       yield payload
--- src/test/scala/io/github/kiberStender/fjwt/decoder/JWTDecoderHeaderTest.scala
+++ src/test/scala/io/github/kiberStender/fjwt/decoder/JWTDecoderHeaderTest.scala
@@ -1,5 +1,6 @@
 package io.github.kiberStender
 package fjwt
+package decoder
 
 import io.github.kiberStender.fjwt.error.JWTError.*
 import cats.syntax.all.{catsSyntaxApplicativeErrorId, catsSyntaxApplicativeId, catsSyntaxOptionId}
@@ -8,29 +9,32 @@ import io.circe.syntax.*
 import io.github.kiberStender.fjwt.crypto.base64.Base64Decoder
 import io.github.kiberStender.fjwt.crypto.hs.HmacEncoder
 import io.github.kiberStender.fjwt.error.JWTError
+import io.github.kiberStender.fjwt.utils.PayloadExtractor
 import org.scalatest.flatspec.AnyFlatSpecLike
 
 import java.time.ZoneId
 
-class JWTDecoderTest extends AnyFlatSpecLike:
+class JWTDecoderHeaderTest extends AnyFlatSpecLike:
   private type F = [T] =>> Either[Throwable, T]
   private lazy val base64Decoder: Base64Decoder[F] = Base64Decoder.dsl
-  private lazy val decoder: JWTDecoder[F] = JWTDecoder.dsl(base64Decoder)
+  private lazy val payloadExtractor: PayloadExtractor[F] = PayloadExtractor.dsl(base64Decoder)
+
+  private lazy val decoder: JWTDecoder[F] = JWTDecoder.dsl(payloadExtractor)
   private given ZoneId = ZoneId.of("UTC")
 
   private final case class Payload(name: String, admin: Boolean)
 
   private given Codec[Payload] = io.circe.generic.semiauto.deriveCodec
 
-  "JWTDecoder" should "decode a JWT token into a Payload object" in {
+  "JWTDecoderHeader" should "decode a JWT token into a Payload object" in {
     // GIVEN
     val expected = Payload("John Doe", true).pure[F]
     val key = "kleber-super-secret-key"
     val input = "eyJhbGciOiJIbWFjU0hBNTEyIiwidHlwIjoiSldUIn0=.eyJpc3MiOm51bGwsInN1YiI6IjEyMzQ1Njc4OTAiLCJhdWQiOm51bGwsImV4cCI6bnVsbCwibmJmIjpudWxsLCJpYXQiOjE1MTYyMzkwMjIsImp0aSI6bnVsbCwicGF5bG9hZCI6eyJuYW1lIjoiSm9obiBEb2UiLCJhZG1pbiI6dHJ1ZX19.29743acd123183547fe568550db9416a02ed3a7aeeb9c4a74ec73baf02bb688b58d58f6508f7f0ab956a22aaf619602359038abae336abf0e0ceb7b0a0268b64"
-    
+
     // WHEN
     val actual = decoder.decode(key)(input)
-    
+
     // THEN
     assert(actual === expected)
   }
@@ -40,10 +44,10 @@ class JWTDecoderTest extends AnyFlatSpecLike:
     val expected = WrongTokenParts.raiseError[F, Payload]
     val key = "kleber-super-secret-key"
     val input = "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0=.d92964cfa2a75550ae735c371a831e4eeb6c40b1734c28b565ab8fbc8a95b038d9e462c0b78a2c1b8fc00117bd0d7eabe92163b738be84e3181aeaede4f7bae"
-    
+
     // WHEN
     val actual = decoder.decode(key)(input)
-    
+
     // THEN
     assert(actual.isLeft)
     assert(actual === expected)
@@ -54,10 +58,10 @@ class JWTDecoderTest extends AnyFlatSpecLike:
     val expected = InvalidSignature.raiseError[F, Payload]
     val key = "kleber-super-secret-key"
     val input = "eyJhbGciOiJIbWFjU0hBNTEyIiwidHlwIjoiSldUIn0=.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0=.d92964cfa2a75550ae735c371a831e4eeb6c40b1734c28b565ab8fbc8a95b038d9e462c0b78a2c1b8fc00117bd0d7eabe92163b738be84e3181aeaede4f7bae"
-    
+
     // WHEN
     val actual = decoder.decode(key)(input)
-    
+
     // THEN
     assert(actual.isLeft)
     assert(actual === expected)
@@ -68,10 +72,10 @@ class JWTDecoderTest extends AnyFlatSpecLike:
     val expected = NotMappedError("exhausted input").raiseError[F, Payload]
     val key = "kleber-super-secret-key"
     val input = "eyJhbGciOiJIbWFjU0hBNTEyIiwidHlwIjoiSldUIn0=.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMg==.9ba31eaf501e464131e4b6ec0ae4ba5bf6ea0b9ebbcd2cce462d3d2fb538e1f279aa1850f6323cf8e95bbbf6a5a592a88f3bd4fa5dd69bc0345c60e9a6ab0f4f"
-    
+
     // WHEN
     val actual = decoder.decode(key)(input)
-    
+
     // THEN
     assert(actual.isLeft)
     assert(actual === expected)
@@ -81,10 +85,10 @@ class JWTDecoderTest extends AnyFlatSpecLike:
     val expected = ExpiredToken.raiseError[F, Payload]
     val key = "kleber-super-secret-key"
     val input = "eyJhbGciOiJIbWFjU0hBNTEyIiwidHlwIjoiSldUIn0=.eyJpc3MiOm51bGwsInN1YiI6IjEyMzQ1Njc4OTAiLCJhdWQiOm51bGwsImV4cCI6MTY2MzUxNzU1MzM0MiwibmJmIjpudWxsLCJpYXQiOjE2NjM1MTgxNTMzNDIsImp0aSI6bnVsbCwicGF5bG9hZCI6eyJuYW1lIjoiSm9obiBEb2UiLCJhZG1pbiI6dHJ1ZX19.1ce2db83ae064b10966a7e6a321358521a3557431bc6690f0787fc5908b357aea03a839839c99673d10f9c5b48793f09a956ef1f9e65c99ee54ede4a2b4fc3a2"
-    
+
     // WHEN
     val actual = decoder.decode(key)(input)
-    
+
     // THEN
     assert(actual.isLeft)
     assert(actual === expected)
