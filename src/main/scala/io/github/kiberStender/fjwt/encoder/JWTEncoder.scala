2cef6c576513f103ce50eda4e087a27f295e8b0e
Kleber Stender <kleber.stender@gmail.com>
2023-01-10 10:19:32 +0100

    Changed Encoder trait to have a generic value for time measurement allowing to have multiple implementations

---
+++ src/main/scala/io/github/kiberStender/fjwt/encoder/JWTEncoder.scala
@@ -0,0 +1,66 @@
+package io.github.kiberStender
+package fjwt
+package encoder
+
+import cats.MonadError
+import cats.syntax.all.{
+  catsSyntaxApplicativeErrorId,
+  catsSyntaxOptionId,
+  toFlatMapOps,
+  toFunctorOps
+}
+import io.circe.Codec
+import io.circe.syntax.EncoderOps
+import io.github.kiberStender.fjwt.crypto.base64.Base64Encoder
+import io.github.kiberStender.fjwt.crypto.hs.HmacEncoder
+import io.github.kiberStender.fjwt.error.JWTError.{EmptyPrivateKey, NullPrivateKey}
+import io.github.kiberStender.fjwt.validation.StringValidation
+
+import io.github.kiberStender.fjwt.utils.toEpoch
+
+import java.time.{LocalDateTime, ZoneId}
+
+/** A trait that describes the JWTEncoder typeclass
+  *
+  * @tparam F
+  *   A given container that wraps the return type
+  * @tparam Time
+  *   The type of time fields: exp, nbf and iat
+  * @tparam P
+  *   The type of the Payload
+  */
+trait JWTEncoder[F[*], Time, P]:
+  /** A method to encode a given payload data described by the type P into a encrypted {@link
+    * String} value
+    *
+    * @param privateKey
+    *   The key that will be used to encode the payload
+    * @param iss
+    *   The issuer of the token[Optional]
+    * @param sub
+    *   The subject of the token[Optional]
+    * @param aud
+    *   The intended audience of the token[Optional]
+    * @param exp
+    *   The expiration time of the token[Optional]
+    * @param nbf
+    *   The not before datetime(The token cannot be used before the given date) of the
+    *   token[Optional]
+    * @param iat
+    *   The Issued at(The time the token was issued) of the token[Optional]
+    * @param jti
+    *   The token ID[Optional]
+    * @param payload
+    *   The data you want to transmit itself
+    * @return
+    *   The encoded {@link String} or an error wrapped in F
+    */
+  def encode(privateKey: String)(
+      iss: Option[String] = None,
+      sub: Option[String] = None,
+      aud: Option[String] = None,
+      exp: Option[Time] = None,
+      nbf: Option[Time] = None,
+      iat: Option[Time] = None,
+      jti: Option[String] = None
+  )(payload: P): F[String]
