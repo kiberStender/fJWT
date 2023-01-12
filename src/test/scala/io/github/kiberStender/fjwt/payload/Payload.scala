1843a3c4bd0bbeff3bcad198aa53ba6c2495e843
Kleber Stender <kleber.stender@gmail.com>
2023-01-12 07:49:19 +0100

    Moved Payload class so all test can use the same Payload class

--- src/test/scala/io/github/kiberStender/fjwt/payload/Payload.scala
+++ src/test/scala/io/github/kiberStender/fjwt/payload/Payload.scala
@@ -1,10 +1,10 @@
 package io.github.kiberStender
 package fjwt
-package encoder
+package payload
 
 import io.circe.Codec
 
-private final case class Payload(name: String, admin: Boolean)
+final case class Payload(name: String, admin: Boolean)
 
 object Payload:
   given Codec[Payload] = io.circe.generic.semiauto.deriveCodec
