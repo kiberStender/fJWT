b5fc39e65215527a5df19ae600a1a25e17455c34
Kleber Stender <kleber.stender@gmail.com>
2023-01-12 07:39:33 +0100

    Formatted documentation

--- src/main/scala/io/github/kiberStender/fjwt/utils/package.scala
+++ src/main/scala/io/github/kiberStender/fjwt/utils/package.scala
@@ -5,10 +5,12 @@ import java.time.{LocalDateTime, ZoneId}
 
 package object utils:
   extension (zoneId: ZoneId) {
-    /**
-     * Method to simplify getting eppoc time from a zone id
-     * @param ldt The {@link LocalDateTime} object to be have epoch time extracted
-     * @return A {@link Long} value containing the date and time formatted in epoch
-     */
+
+    /** Method to simplify getting eppoc time from a zone id
+      * @param ldt
+      *   The {@link LocalDateTime} object to be have epoch time extracted
+      * @return
+      *   A {@link Long} value containing the date and time formatted in epoch
+      */
     def toEpoch(ldt: LocalDateTime): Long = ldt.atZone(zoneId).toInstant.toEpochMilli
   }
