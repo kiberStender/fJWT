b8c821ca0c6b0536169a22ba30747d5d6176282c
Kleber Stender <kleber.stender@gmail.com>
2023-11-05 09:16:29 +0100

    Added new algorithms support

--- src/main/scala/io/github/kiberStender/fjwt/model/Alg.scala
+++ src/main/scala/io/github/kiberStender/fjwt/model/Alg.scala
@@ -26,6 +26,8 @@ object Alg:
   }
   extension (alg: Alg) {
     def toHmacAlgorithms: HmacEncoderAlgorithms = alg match {
+      case Alg("HS1", _) => HmacSHA1
+      case Alg("HS224", _) => HmacSHA224
       case Alg("HS256", _) => HmacSHA256
       case Alg("HS384", _) => HmacSHA384
       case Alg("HS512", _) => HmacSHA512
